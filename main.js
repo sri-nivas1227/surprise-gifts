23bc990e3c242a0b21db3239fc0f1c43a2167a0f
Sri-1227 <srinivasmekala1227@gmail.com>
2022-04-30 20:46:25 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-const giftBtn = document.querySelector(".get-gifts")
-document.querySelector(".main").addEventListener("click", function(){
+const giftBtn = document.querySelector(".get-gifts");
+const displaygif = document.querySelector(".gif-img");
+document.querySelector(".main").addEventListener("click", function () {
+  giftBtn.style.display = "initial";
+});
 
-    giftBtn.style.display = "initial";
-})
+giftBtn.addEventListener("click", function () {
+  let name = prompt("Name your gift: ");
+  console.log(name);
+  if (name == "girlfriend") {
+    confirm("You sure you want a girlfriend?");
+    displaygif.src = "laugh.gif";
+  } else if (name == "puppy") {
+    displaygif.src = "puppy.gif";
+  } else if (name == "mobile") {
+    displaygif.src = "mobile.gif";
+  } else {
+    alert("Oops, Out of stock");
+    displaygif.src = "./oops.gif";
+  }
+});
